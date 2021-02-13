4a0a50f781d2027111d2a6c54f06a95034b33bd1
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-13 15:39:55 +0100

    installed and configured sequelize

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
 
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+
     consign()
-        .include('controllers')
+        .include('./api/controllers')
         .into(app)
 
     return app
