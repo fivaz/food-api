fb4b9440f78975e1989b2bafce4fcd5272fc7aa5
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-13 14:41:18 +0100

    express | consign installed | first route

---
+++ index.js
@@ -0,0 +1,7 @@
+const customExpress = require('./config/customExpress')
+const app = customExpress()
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+    console.log(`Server running in the port ${port}`));
