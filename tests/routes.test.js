f256381e29798fdb611d0790675fc8095aa1c519
fivaz <31249205+fivaz@users.noreply.github.com>
2021-04-08 11:45:56 +0200

    test: end point tests for ingredients done

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,52 +1,56 @@
 const request = require('supertest');
 const app = require('../index');
 
-const ingredientsURL = '/ingredients/';
+const ingredientsURL = '/ingredients';
+
+// TODO stop the process after tests are completed in webstorm test runner
 
 describe('Ingredient API', () => {
     it('should show all ingredients', async () => {
-        const res = await request(app).get(ingredientsURL);
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveLength(7);
+        const response = await request(app).get(ingredientsURL);
+        expect(response.statusCode).toBe(200);
+        //TODO check how test the database later
+        // expect(response.body).toHaveLength(7);
     });
     it('should show an ingredient', async () => {
-        const res = await request(app).get(`${ingredientsURL}1`);
+        const res = await request(app).get(`${ingredientsURL}/1`);
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('name');
     });
     it('shouldn\'t show an ingredient', async () => {
-        const res = await request(app).get(`${ingredientsURL}10`);
+        const res = await request(app).get(`${ingredientsURL}/10`);
         expect(res.statusCode).toEqual(200);
         expect(res.body).toBeNull();
     });
-    // ,
-//     // it('should create a new ingredient', async () => {
-//     //     const res = await request(app)
-//     //         .post(ingredientsURL)
-//     //         .send({
-//     //             firstName: 'Bob',
-//     //             lastName: 'Doe',
-//     //             email: 'bob@doe.com',
-//     //             password: '12345678'
-//     //         });
-//     //     expect(res.statusCode).toEqual(201);
-//     //     expect(res.body).toHaveProperty('ingredient');
-//     // }),
-//     // it('should update a ingredient', async () => {
-//     //     const res = await request(app)
-//     //         .put('/api/ingredients/3')
-//     //         .send({
-//     //             firstName: 'Bob',
-//     //             lastName: 'Smith',
-//     //             email: 'bob@doe.com',
-//     //             password: 'abc123'
-//     //         });
-//     //     expect(res.statusCode).toEqual(200);
-//     //     expect(res.body).toHaveProperty('ingredient');
-//     // }),
-//     // it('should delete a ingredient', async () => {
-//     //     const res = await request(app)
-//     //         .del('/api/ingredients/3');
-//     //     expect(res.statusCode).toEqual(204);
-//     // });
+    it('should create a new ingredient', async () => {
+        const res = await request(app)
+            .post(ingredientsURL)
+            .send({
+                name: 'Butter',
+                isCountable: true,
+                price: 3.25,
+                unit: 'g',
+                quantity: 200,
+            });
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('name');
+    });
+    it('should update a ingredient', async () => {
+        const res = await request(app)
+            .put(`${ingredientsURL}/6`)
+            .send({
+                name: 'Milk',
+                isCountable: true,
+                price: 1.55,
+                unit: 'l',
+                quantity: 1.5,
+            });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('name');
+    });
+    it('should delete a ingredient', async () => {
+        const res = await request(app)
+            .del(`${ingredientsURL}/4`);
+        expect(res.statusCode).toEqual(200);
+    });
 });
