facb55c5e837bf11ca72327d08f722f0f772b4c9
fivaz <31249205+fivaz@users.noreply.github.com>
2021-04-11 00:55:56 +0200

    feature: meal CRUD implemented

--- api/controllers/MealController.js
+++ api/controllers/MealController.js
@@ -12,6 +12,45 @@ class MealController {
         .json(error.message);
     }
   }
+
+  // TODO make all create return a full object
+  static async create(req, res) {
+    try {
+      const { id } = await Meal.create(req.body);
+      const meal = await Meal.scope(['defaultScope', 'full'])
+        .findByPk(id);
+      return res.status(201)
+        .json(meal);
+    } catch (error) {
+      return res.status(500)
+        .json(error.message);
+    }
+  }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    try {
+      await Meal.update(req.body, { where: { id } });
+      const meal = await Meal.scope(['defaultScope', 'full'])
+        .findByPk(id);
+      return res.status(200)
+        .json(meal);
+    } catch (error) {
+      return res.status(500)
+        .json(error.message);
+    }
+  }
+
+  static async delete(req, res) {
+    try {
+      await Meal.destroy({ where: { id: req.params.id } });
+      return res.status(200)
+        .json('meal removed');
+    } catch (error) {
+      return res.status(500)
+        .json(error.message);
+    }
+  }
 }
 
 module.exports = MealController;
--- api/routes/mealRoute.js
+++ api/routes/mealRoute.js
@@ -4,5 +4,8 @@ const MealController = require('../controllers/MealController');
 const router = Router();
 
 router.get('/meals', MealController.getAll);
+router.post('/meals', MealController.create);
+router.put('/meals/:id', MealController.update);
+router.delete('/meals/:id', MealController.delete);
 
 module.exports = router;
