433748da4e577e1becf1b929956a8c1077da2d36
fivaz <31249205+fivaz@users.noreply.github.com>
2021-04-21 21:52:00 +0200

    refactor: changing line breaks

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Controller = require('./Controller');
 const { User } = require('../models');
@@ -6,29 +5,6 @@ const { User } = require('../models');
 const { TOKEN_SECRET } = require('../helpers/vars');
 
 class UserController extends Controller {
-  // static async getAll(req, res) {
-  //   try {
-  //     const users = await User.findAll();
-  //     return res.status(200)
-  //       .json(users);
-  //   } catch (error) {
-  //     return res.status(500)
-  //       .json(error.message);
-  //   }
-  // }
-  //
-  // static async get(req, res) {
-  //   const { id } = req.params;
-  //   try {
-  //     const user = await User.findByPk(id);
-  //     return res.status(200)
-  //       .json(user);
-  //   } catch (error) {
-  //     return res.status(500)
-  //       .json(error.message);
-  //   }
-  // }
-
   static convertUser(userModel) {
     const user = { ...userModel.toJSON(), password: undefined };
     user.token = jwt.sign(user, TOKEN_SECRET);
@@ -62,19 +38,6 @@ class UserController extends Controller {
         .json(error.stack);
     }
   }
-
-  // static async update(req, res) {
-  //   const { id } = req.params;
-  //   try {
-  //     await User.update(req.body, { where: { id } });
-  //     const user = await User.findByPk(id);
-  //     return res.status(200)
-  //       .json(user);
-  //   } catch (error) {
-  //     return res.status(500)
-  //       .json(error.message);
-  //   }
-  // }
 }
 
 module.exports = UserController;
